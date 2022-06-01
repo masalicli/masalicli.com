4860a27598d0474ed4bf1dd28d0caa912df3c70d
Yaşar İçli <yasaricli@gmail.com>
2022-06-01 12:40:09 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
     <div>
-      Hi my name is Masal (Fairy Tale) and I'm 6 years old.
+      Hi my name is Masal (Fairy Tale) and I'm 6 years old
     </div>
   );
 }
